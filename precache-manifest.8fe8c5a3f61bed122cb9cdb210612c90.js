25d544f1c83f8dc74b04ceb2b5fec893078d8bce
Brian Balayon <brianbalayon@gmail.com>
2021-04-06 01:56:32 -0400

    Updates

---
+++ precache-manifest.8fe8c5a3f61bed122cb9cdb210612c90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6356b87e989ce906b127256b727f49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8f5a524669933ad7c8d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a8015f881300b061d81",
+    "url": "/static/js/2.c2064466.chunk.js"
+  },
+  {
+    "revision": "e11c9522225ea1ecbe5ba3062b375710",
+    "url": "/static/js/2.c2064466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4252bc7eb74622c639",
+    "url": "/static/js/3.a3f17a56.chunk.js"
+  },
+  {
+    "revision": "a8f5a524669933ad7c8d",
+    "url": "/static/js/main.96658d9c.chunk.js"
+  },
+  {
+    "revision": "943c944c480856f34e69",
+    "url": "/static/js/runtime-main.dad4e130.js"
+  },
+  {
+    "revision": "1d439eb607a8259e3c71cb3d1c877097",
+    "url": "/static/media/BrianBalayon.1d439eb6.png"
+  }
+]);
